5e9f667eec4157d5b1ed3f136774443586d2f692
Jano Bokuchava <https://github.com/login/oauth/authorize?state=socketId%3A50e1b855-ef2e-41fe-ac81-bf197b50c84e%3Baction%3Alogin%3BinApp%3Atrue%3BneedsGKPermissions%3Atrue%3Bchallenge%3A0f7cfbf0-9c29-49b8-8d88-7d83fb0a3cb9%3B&scope=admin%3Apublic_key%2Crepo%2Cworkflow%2Cread%3Aorg%2Cuser%3Aemail&response_type=code&client_id=a7557949433b7d282a76>
2021-04-12 20:26:37 +0400

    update auth routing

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-const routes: Routes = [{ path: '', component: AuthComponent }];
+const routes: Routes = [
+  { path: '', component: AuthComponent },
+  {
+    path: 'sign-up',
+    component: SignUpComponent,
+  },
+  {
+    path: 'sign-in',
+    component: SignInComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
