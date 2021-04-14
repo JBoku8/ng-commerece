5e08bff23e25ca36739baaf092fc223d31782305
Jano Bokuchava <https://github.com/login/oauth/authorize?state=socketId%3A50e1b855-ef2e-41fe-ac81-bf197b50c84e%3Baction%3Alogin%3BinApp%3Atrue%3BneedsGKPermissions%3Atrue%3Bchallenge%3A0f7cfbf0-9c29-49b8-8d88-7d83fb0a3cb9%3B&scope=admin%3Apublic_key%2Crepo%2Cworkflow%2Cread%3Aorg%2Cuser%3Aemail&response_type=code&client_id=a7557949433b7d282a76>
2021-04-14 19:59:08 +0400

    fixes: routing issiues

--- src/app/shopping-cart/cart/cart.component.ts
+++ src/app/shopping-cart/cart/cart.component.ts
@@ -5,19 +5,16 @@ import { ProductService } from 'src/app/shopping-cart/services/product.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
-
   products: Product[] = [];
-  constructor(private productService : ProductService) {}
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-    this.productService.getProducts().subscribe((resp)=>{
+    this.productService.getProducts().subscribe((resp) => {
       this.products = resp;
-      console.log(this.products)
+      console.log(this.products);
     });
   }
-
-
 }
