f52322264392e96b7118469a15fe40e22d92586a
NatiaChubinidze <72444557+NatiaChubinidze@users.noreply.github.com>
2021-04-14 19:07:36 +0400

    firebase auth (#7)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
+
 const routes: Routes = [
   {
     path: 'admin',
@@ -15,6 +18,16 @@ const routes: Routes = [
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
   },
+  //auth-routing.module-ში გადავიტანე ეს ნაწილი, მაგრამ არ იმუშავა. ამიტომ აქ დავტოვე ისევ.
+  {
+    path:'forgotPassword',
+    component:ForgotPasswordComponent
+  },
+  {
+    path:'resetPassword',
+    component:ResetPasswordComponent
+  },
+  //*************************************************************************************** */
   {
     path: '*',
     redirectTo: 'user',
--- src/app/auth/reset-password/reset-password.component.ts
+++ src/app/auth/reset-password/reset-password.component.ts
@@ -1,15 +1,76 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+import { MustMatch } from '../shared/validators/passwords-match.validator';
+import { firebaseAuthService } from '../shared/services/firebase-auth.service';
+
 
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
-  styleUrls: ['./reset-password.component.scss']
+  styleUrls: ['./reset-password.component.scss'],
 })
 export class ResetPasswordComponent implements OnInit {
+  oldPassword:FormControl;
+  password: FormControl;
+  confirmPassword: FormControl;
+  resetForm: FormGroup;
+  buttonHover: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    public _firebaseAuth: firebaseAuthService,
+    private formBuilder: FormBuilder,
+  ) {
+    this.oldPassword=new FormControl('',Validators.required);
+    this.password = new FormControl(
+      '',
+      Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(20),
+        Validators.pattern(
+          '(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-zd$@$!%*?&].{5,}',
+        ),
+      ]),
+    );
+    this.confirmPassword = new FormControl('', Validators.required);
+    this.resetForm = this.formBuilder.group(
+      { oldPassword:this.oldPassword,
+        password: this.password,
+        confirmPassword: this.confirmPassword,
+      },
+      { validators: MustMatch('password', 'confirmPassword') },
+    );
   }
+
+  oldPasswordIsInvalid():boolean{
+    return this.oldPassword.invalid && (this.oldPassword.touched || this.buttonHover);
+  }
+  
+  passwordIsInvalid(): boolean {
+    return this.password.invalid && (this.password.touched || this.buttonHover);
+  }
+
+  confirmPasswordIsInvalid(): boolean {
+    return (
+      this.confirmPassword.invalid &&
+      ((this.confirmPassword.touched &&
+        this.password.value &&
+        this.confirmPassword.value !== this.password.value) ||
+        this.buttonHover)
+    );
+  }
+
+  resetPassword(): void {
+    this._firebaseAuth.resetPassword(this.oldPassword.value,this.password.value);
+      this.resetForm.reset();
+  }
+
+  ngOnInit(): void {}
 
 }
